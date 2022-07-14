04b4bc351141583ea01271bfc595f3d5d2380858
Murilo Olegini <muriloolegini@outlook.com>
2022-07-14 14:58:07 -0300

    Initial commit

---
+++ ipexterno.py
@@ -0,0 +1,17 @@
+import re
+import json
+from urllib.request import urlopen
+
+url = 'https://ipinfo.io/json'
+
+resposta = urlopen(url)
+dados = json.load(resposta)
+
+# ip = dados['ip'] #
+org = dados['org']
+cid = dados['city']
+pais = dados['country']
+regiao = dados['region']
+
+print('Detalhes do IP externo\n')
+print('\nRegião: {1}\nPaís: {2}\nCidade: {3}\nOrganização: {0}'.format(org, regiao, pais, cid))
